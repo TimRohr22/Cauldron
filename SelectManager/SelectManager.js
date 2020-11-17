ab8ad0ced0ab50e01d2d9edece3c5aed67fb870a
timmaugh <timrohr22@gmail.com>
2020-11-17 08:29:17 -0500

    Version 0.0.3

--- SelectManager/SelectManager.js
+++ SelectManager/SelectManager.js
@@ -3,21 +3,30 @@
 Name			:	SelectManager
 GitHub			:   https://github.com/TimRohr22/Cauldron/tree/master/SelectManager
 Roll20 Contact	:	timmaugh && TheAaron
-Version			:	0.0.2
-Last Update		:	11/16/2020
+Version			:	0.0.3
+Last Update		:	11/17/2020
 =========================================================
 
 ABSTRACT: Preserve the selected property of the message object from any user generated api call
           so it is available to any other script called *from* the API
           (api-generated message objects have no selected property)
+
+IMPLEMENTION IN OTHER SCRIPTS:
+1) Place these lines anywhere in the top scope of your script:
+    let getSelected = () => [];
+    on('ready', () => { if(undefined !== typeof SelectManager) getSelected = () => SelectManager.GetSelected(); });
+
+2) Place this line within the on('chat:message') handler, immediately after testing the api handle:
+    if('API' === msg.playerid) msg.selected = getSelected();
+
 */
 
 const SelectManager = (() => {
     // ==================================================
     //		VERSION
     // ==================================================
-    const vrs = '0.0.2';
-    const vd = new Date(1605583654762);
+    const vrs = '0.0.3';
+    const vd = new Date(1605619625605);
     const versionInfo = () => {
         log('\u0166\u0166 SelectManager v' + vrs + ', ' + vd.getFullYear() + '/' + (vd.getMonth() + 1) + '/' + vd.getDate() + ' \u0166\u0166');
         return;
