4727da914e795d4f194d187685934558790de8d4
timmaugh <timrohr22@gmail.com>
2022-11-15 22:21:01 -0500

    checkLightLevel Integration

--- Fetch/Fetch.js
+++ Fetch/Fetch.js
@@ -1,27 +1,27 @@
+/* eslint no-prototype-builtins: "off" */
 /*
 =========================================================
 Name			:	Fetch
 GitHub			:	https://github.com/TimRohr22/Cauldron/tree/master/Fetch
-Roll20 Contact	:	timmaugh
-Version			:   2.0.b3
-Last Update		:	9/28/2022
+Roll20 Contact  :	timmaugh
+Version			:   2.0.0
+Last Update		:	11/15/2022
 =========================================================
 */
 var API_Meta = API_Meta || {};
 API_Meta.Fetch = { offset: Number.MAX_SAFE_INTEGER, lineCount: -1 };
-{
-    try { throw new Error(''); } catch (e) { API_Meta.Fetch.offset = (parseInt(e.stack.split(/\n/)[1].replace(/^.*:(\d+):.*$/, '$1'), 10) - (13)); }
-}
+{ try { throw new Error(''); } catch (e) { API_Meta.Fetch.offset = (parseInt(e.stack.split(/\n/)[1].replace(/^.*:(\d+):.*$/, '$1'), 10) - (13)); } }
 
-const Fetch = (() => {
+const Fetch = (() => { //eslint-disable-line no-unused-vars
     const apiproject = 'Fetch';
-    const version = '2.0.b2';
+    const version = '2.0.0';
+    const apilogo = 'https://i.imgur.com/jeIkjvS.png';
     const schemaVersion = 0.1;
     API_Meta[apiproject].version = version;
-    const vd = new Date(1664399284476);
+    const vd = new Date(1668568195650);
     const versionInfo = () => {
         log(`\u0166\u0166 ${apiproject} v${API_Meta[apiproject].version}, ${vd.getFullYear()}/${vd.getMonth() + 1}/${vd.getDate()} \u0166\u0166 -- offset ${API_Meta[apiproject].offset}`);
-        if (!state.hasOwnProperty(apiproject) || state[apiproject].version !== schemaVersion) {
+        if (!state.hasOwnProperty(apiproject) || state[apiproject].version !== schemaVersion) { //eslint-disable-line no-prototype-builtins
             log(`  > Updating ${apiproject} Schema to v${schemaVersion} <`);
             switch (state[apiproject] && state[apiproject].version) {
 
@@ -180,8 +180,8 @@ const Fetch = (() => {
         }
 
         let filterLib = {
-            '=': (a) => a.contents[0] == a.contents[1],
-            '!=': (a) => a.contents[0] != a.contents[1],
+            '=': (a) => a.contents[0] == a.contents[1], // eslint-disable-line eqeqeq
+            '!=': (a) => a.contents[0] != a.contents[1],// eslint-disable-line eqeqeq
             '~': (a) => a.contents[0].includes(a.contents[1]),
             '!~': (a) => !a.contents[0].includes(a.contents[1]),
             '>': (a) => (internalTestLib.num(a.contents[0]) ? Number(a.contents[0]) : a.contents[0]) > (internalTestLib.num(a.contents[1]) ? Number(a.contents[1]) : a.contents[1]),
@@ -286,7 +286,7 @@ const Fetch = (() => {
         let qrx = new RegExp(escapeRegExp(query), 'i');
         let charsIControl = findObjs({ type: 'character' });
         charsIControl = playerIsGM(pid) ? charsIControl : charsIControl.filter(c => {
-            return c.get('controlledby').split(',').reduce((m, p, i) => {
+            return c.get('controlledby').split(',').reduce((m, p) => {
                 return m || p === 'all' || p === pid;
             }, false)
         });
@@ -338,7 +338,7 @@ const Fetch = (() => {
         token = getToken(t);
         if (!token) return;
         token = simpleObj(token);
-        if (!tokenStatuses.hasOwnProperty(token.id) || tokenStatuses[token.id].msgId !== msgId) {
+        if (!tokenStatuses.hasOwnProperty(token.id) || tokenStatuses[token.id].msgId !== msgId) { // eslint-disable-line no-prototype-builtins
             tokenStatuses[token.id] = new StatusBlock({ token: token, msgId: msgId });
         }
         rxret = /(?<marker>.+?)(?:\?(?<index>\d+|all\+?))?$/.exec(query);
@@ -365,7 +365,6 @@ const Fetch = (() => {
                     m.count = m.count || statusblock.length;
                     return m;
                 }, {});
-                break;
             case 'all+':
                 return statusblock.reduce((m, sm) => {
                     m.num = `${Number(m.num || 0) + Number(sm.num)}`;
@@ -376,7 +375,6 @@ const Fetch = (() => {
                     m.count = m.count || statusblock.length;
                     return m;
                 }, {});
-                break;
             default:
                 if (statusblock.length >= modindex) {
                     return Object.assign({}, statusblock[modindex - 1], { count: index ? '1' : statusblock.length });
@@ -403,11 +401,20 @@ const Fetch = (() => {
         return retval;
     };
     const getToken = (info, pgid = '') => {
+        let lightvals = {
+            base: {},
+            assign: {}
+        };
         let token = findObjs({ type: 'graphic', subtype: 'token', id: info })[0] ||
             findObjs({ type: 'graphic', subtype: 'card', id: info })[0] ||
             findObjs({ type: 'graphic', subtype: 'token', name: info, pageid: pgid })[0];
         if (token && token.id) {
-            token = Object.assign(simpleObj(token), getTrackerVal(token));
+            if (typeof checkLightLevel !== 'undefined' && checkLightLevel.hasOwnProperty('isLitBy') && typeof checkLightLevel.isLitBy === 'function') {
+                lightvals.base = checkLightLevel.isLitBy(token);
+                lightvals.assign.checklight_isbright = lightvals.base.bright ? 'true' : 'false';
+                lightvals.assign.checklight_total = lightvals.base.total
+            }
+            token = Object.assign(simpleObj(token), getTrackerVal(token),lightvals.assign);
         }
         return token;
     };
@@ -489,6 +496,10 @@ const Fetch = (() => {
         bar3_current: { refersto: 'bar3_value', permissionsreq: 'any', dataval: (d) => d },
         bar3_value: { refersto: 'bar3_value', permissionsreq: 'any', dataval: (d) => d },
         bright_light_distance: { refersto: 'bright_light_distance', permissionsreq: 'any', dataval: (d) => d },
+
+        checklight_isbright: { refersto: 'checklight_isbright', permissionsreq: 'any', dataval: (d) => d },
+        checklight_total: { refersto: 'checklight_total', permissionsreq: 'any', dataval: (d) => d },
+
         compact_bar: { refersto: 'compact_bar', permissionsreq: 'any', dataval: (d) => d },
         token_cby: { refersto: 'controlledby', permissionsreq: 'any', dataval: (d) => d },
         token_controlledby: { refersto: 'controlledby', permissionsreq: 'any', dataval: (d) => d },
@@ -768,12 +779,11 @@ const Fetch = (() => {
         const filterObj = {
             'page': (t) => t._pageid === (Campaign().get('playerspecificpages')[msg.playerid] || Campaign().get('playerpageid')),
             'ribbon': (t) => t._pageid === Campaign().get('playerpageid'),
-            'gm': (t) => true
+            'gm': () => true
         };
         const propContainers = {
             token: tokenProps,
             character: charProps,
-            player: playerProps,
             page: pageProps,
             campaign: campaignProps,
             marker: markerProps,
@@ -873,7 +883,7 @@ const Fetch = (() => {
                                     getMarker(/(?<marker>.+?)(?:\?(?<index>\d+|all\+?))?$/.exec(identikey.toLowerCase())[1]).name &&
                                     Object.keys(statusProps).includes((subprop || 'value').toLowerCase())) {
                                     presource = simpleObj(getToken(msg.selected[0]._id));
-                                    if (!tokenStatuses.hasOwnProperty(presource.id) || tokenStatuses[presource.id].msgId !== msgId) {
+                                    if (!tokenStatuses.hasOwnProperty(presource.id) || tokenStatuses[presource.id].msgId !== msgId) {// eslint-disable-line no-prototype-builtins
                                         tokenStatuses[presource.id] = new StatusBlock({ token: presource, msgId: msgId });
                                     }
                                     source = getStatus(msg.selected[0]._id, identikey, msgId);
@@ -905,8 +915,8 @@ const Fetch = (() => {
                                     getMarker(/(?<marker>.+?)(?:\?(?<index>\d+|all\+?))?$/.exec(identikey.toLowerCase())[1]).name &&
                                     Object.keys(statusProps).includes((subprop || 'value').toLowerCase())) {
                                     presource = simpleObj(getToken(presource.id, presource._pageid));
-                                    if (presource && !presource.hasOwnProperty('id')) presource.id = presource._id;
-                                    if (!tokenStatuses.hasOwnProperty(presource.id) || tokenStatuses[presource.id].msgId !== msgId) {
+                                    if (presource && !presource.hasOwnProperty('id')) presource.id = presource._id; // eslint-disable-line no-prototype-builtins
+                                    if (!tokenStatuses.hasOwnProperty(presource.id) || tokenStatuses[presource.id].msgId !== msgId) { // eslint-disable-line no-prototype-builtins
                                         tokenStatuses[presource.id] = new StatusBlock({ token: presource, msgId: msgId });
                                     }
                                     source = getStatus(presource.id, identikey, msgId);
@@ -979,7 +989,7 @@ const Fetch = (() => {
                             retval = def;
                         } else {
                             presource = to[(reverse ? to.length - (offset % to.length) : offset % to.length) % to.length];
-                            if (presource && !presource.hasOwnProperty('id')) presource.id = presource._id;
+                            if (presource && !presource.hasOwnProperty('id')) presource.id = presource._id; // eslint-disable-line no-prototype-builtins
                             source = simpleObj(getChar(presource.id, msg.playerid));
                         }
                         break;
@@ -1019,6 +1029,75 @@ const Fetch = (() => {
         return condensereturn(funcret, status, notes);
     };
 
+    const checkDependencies = (deps) => {
+        /* pass array of objects like
+            { name: 'ModName', version: '#.#.#' || '', mod: ModName || undefined, checks: [ [ExposedItem, type], [ExposedItem, type] ] }
+        */
+        const dependencyEngine = (deps) => {
+            const versionCheck = (mv, rv) => {
+                let modv = [...mv.split('.'), ...Array(4).fill(0)].slice(0, 4);
+                let reqv = [...rv.split('.'), ...Array(4).fill(0)].slice(0, 4);
+                return reqv.reduce((m, v, i) => {
+                    if (m.pass || m.fail) return m;
+                    if (i < 3) {
+                        if (parseInt(modv[i]) > parseInt(reqv[i])) m.pass = true;
+                        else if (parseInt(modv[i]) < parseInt(reqv[i])) m.fail = true;
+                    } else {
+                        // all betas are considered below the release they are attached to
+                        if (reqv[i] === 0 && modv[i] === 0) m.pass = true;
+                        else if (modv[i] === 0) m.pass = true;
+                        else if (reqv[i] === 0) m.fail = true;
+                        else if (parseInt(modv[i].slice(1)) >= parseInt(reqv[i].slice(1))) m.pass = true;
+                    }
+                    return m;
+                }, { pass: false, fail: false }).pass;
+            };
+
+            let result = { passed: true, failures: {}, optfailures: {} };
+            deps.forEach(d => {
+                let failObj = d.optional ? result.optfailures : result.failures;
+                if (!d.mod) {
+                    if (!d.optional) result.passed = false;
+                    failObj[d.name] = 'Not found';
+                    return;
+                }
+                if (d.version && d.version.length) {
+                    if (!(API_Meta[d.name].version && API_Meta[d.name].version.length && versionCheck(API_Meta[d.name].version, d.version))) {
+                        if (!d.optional) result.passed = false;
+                        failObj[d.name] = `Incorrect version. Required v${d.version}. ${API_Meta[d.name].version && API_Meta[d.name].version.length ? `Found v${API_Meta[d.name].version}` : 'Unable to tell version of current.'}`;
+                        return;
+                    }
+                }
+                d.checks.reduce((m, c) => {
+                    if (!m.passed) return m;
+                    let [pname, ptype] = c;
+                    if (!d.mod.hasOwnProperty(pname) || typeof d.mod[pname] !== ptype) {
+                        if (!d.optional) m.passed = false;
+                        failObj[d.name] = `Incorrect version.`;
+                    }
+                    return m;
+                }, result);
+            });
+            return result;
+        };
+        let depCheck = dependencyEngine(deps);
+        let failures = '', contents = '', msg = '';
+        if (Object.keys(depCheck.optfailures).length) { // optional components were missing
+            failures = Object.keys(depCheck.optfailures).map(k => `&bull; <code>${k}</code> : ${depCheck.optfailures[k]}`).join('<br>');
+            contents = `<span style="font-weight: bold">${apiproject}</span> utilizies one or more other scripts for optional features, and works best with those scripts installed. You can typically find these optional scripts in the 1-click Mod Library:<br>${failures}`;
+            msg = `<div style="width: 100%;border: none;border-radius: 0px;min-height: 60px;display: block;text-align: left;white-space: pre-wrap;overflow: hidden"><div style="font-size: 14px;font-family: &quot;Segoe UI&quot;, Roboto, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif"><div style="background-color: #000000;border-radius: 6px 6px 0px 0px;position: relative;border-width: 2px 2px 0px 2px;border-style:  solid;border-color: black;"><div style="border-radius: 18px;width: 35px;height: 35px;position: absolute;left: 3px;top: 2px;"><img style="background-color: transparent ; float: left ; border: none ; max-height: 40px" src="${typeof apilogo !== 'undefined' ? apilogo : 'https://i.imgur.com/kxkuQFy.png'}"></div><div style="background-color: #c94d4d;font-weight: bold;font-size: 18px;line-height: 36px;border-radius: 6px 6px 0px 0px;padding: 4px 4px 0px 43px;color: #ffffff;min-height: 38px;">MISSING MOD DETECTED</div></div><div style="background-color: white;padding: 4px 8px;border: 2px solid #000000;border-bottom-style: none;color: #404040;">${contents}</div><div style="background-color: white;text-align: right;padding: 4px 8px;border: 2px solid #000000;border-top-style: none;border-radius: 0px 0px 6px 6px"></div></div></div>`;
+            sendChat(apiproject, `/w gm ${msg}`);
+        }
+        if (!depCheck.passed) {
+            failures = Object.keys(depCheck.failures).map(k => `&bull; <code>${k}</code> : ${depCheck.failures[k]}`).join('<br>');
+            contents = `<span style="font-weight: bold">${apiproject}</span> requires other scripts to work. Please use the 1-click Mod Library to correct the listed problems:<br>${failures}`;
+            msg = `<div style="width: 100%;border: none;border-radius: 0px;min-height: 60px;display: block;text-align: left;white-space: pre-wrap;overflow: hidden"><div style="font-size: 14px;font-family: &quot;Segoe UI&quot;, Roboto, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif"><div style="background-color: #000000;border-radius: 6px 6px 0px 0px;position: relative;border-width: 2px 2px 0px 2px;border-style:  solid;border-color: black;"><div style="border-radius: 18px;width: 35px;height: 35px;position: absolute;left: 3px;top: 2px;"><img style="background-color: transparent ; float: left ; border: none ; max-height: 40px" src="${typeof apilogo !== 'undefined' ? apilogo : 'https://i.imgur.com/kxkuQFy.png'}"></div><div style="background-color: #c94d4d;font-weight: bold;font-size: 18px;line-height: 36px;border-radius: 6px 6px 0px 0px;padding: 4px 4px 0px 43px;color: #ffffff;min-height: 38px;">MISSING MOD DETECTED</div></div><div style="background-color: white;padding: 4px 8px;border: 2px solid #000000;border-bottom-style: none;color: #404040;">${contents}</div><div style="background-color: white;text-align: right;padding: 4px 8px;border: 2px solid #000000;border-top-style: none;border-radius: 0px 0px 6px 6px"></div></div></div>`;
+            sendChat(apiproject, `/w gm ${msg}`);
+            return false;
+        }
+        return true;
+    };
+
 
     let scriptisplugin = false;
     const fetch = (m, s) => handleInput(m, s);
@@ -1027,14 +1106,31 @@ const Fetch = (() => {
         versionInfo();
         logsig();
 
+        let reqs = [
+            {
+                name: 'checkLightLevel',
+//                version: `1.0.0.b3`,
+                mod: typeof checkLightLevel !== 'undefined' ? checkLightLevel : undefined,
+                checks: [['isLitBy', 'function']],
+                optional: true
+            },
+            {
+                name: 'libTokenMarkers',
+                version: `0.1.2`,
+                mod: typeof libTokenMarkers !== 'undefined' ? libTokenMarkers : undefined,
+                checks: [['getStatus', 'function'], ['getStatuses', 'function'], ['getOrderedList', 'function']]
+            }
+        ];
+        if (!checkDependencies(reqs)) return;
+
         if ('undefined' === typeof libTokenMarkers
             || (['getStatus', 'getStatuses', 'getOrderedList'].find(k =>
-                !libTokenMarkers.hasOwnProperty(k) || 'function' !== typeof libTokenMarkers[k]
+                !libTokenMarkers.hasOwnProperty(k) || 'function' !== typeof libTokenMarkers[k] // eslint-disable-line no-prototype-builtins
             ))
         ) {
             // blow up
             let msg = `<div style="width: 100%;border: none;border-radius: 0px;min-height: 60px;display: block;text-align: left;white-space: pre-wrap;overflow: hidden"><div style="font-size: 14px;font-family: &quot;Segoe UI&quot;, Roboto, Ubuntu, Cantarell, &quot;Helvetica Neue&quot;, sans-serif"><div style="background-color: #000000;border-radius: 6px 6px 0px 0px;position: relative;border-width: 2px 2px 0px 2px;border-style:  solid;border-color: black;"><div style="border-radius: 18px;width: 35px;height: 35px;position: absolute;left: 3px;top: 2px;"><img style="background-color: transparent ; float: left ; border: none ; max-height: 40px" src="https://i.imgur.com/jeIkjvS.png"></div><div style="background-color: #c94d4d;font-weight: bold;font-size: 18px;line-height: 36px;border-radius: 6px 6px 0px 0px;padding: 4px 4px 0px 43px;color: #ffffff;min-height: 38px;">MISSING MOD</div></div><div style="background-color: white;padding: 4px 8px;border: 2px solid #000000;border-bottom-style: none;color: #404040;"><span style="font-weight: bold">Fetch</span> requires <code>libTokenMarkers</code>.  Please install it from the 1-click Mod Library.</div><div style="background-color: white;text-align: right;padding: 4px 8px;border: 2px solid #000000;border-top-style: none;border-radius: 0px 0px 6px 6px"></div></div></div>`;
-            sendChat('TestScript', `/w gm ${msg}`);
+            sendChat('Fetch', `/w gm ${msg}`);
             return;
         }
 
