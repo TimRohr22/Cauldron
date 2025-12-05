3540eaedac13b272c863171105871cd069ce1a68
timmaugh <timrohr22@gmail.com>
2025-12-05 09:48:12 -0500

    Simon Game Update
    Assemble and SelectManager get new features to allow more functionality around menu chatting (Assemble) and selecting tokens (SelectManager) to make the Simon-styled game request possible with the best gameplay.

--- Assemble/1.0.0/Assemble.js
+++ Assemble/1.0.0/Assemble.js
@@ -3,8 +3,8 @@
 Name            :   Assemble
 GitHub          :   
 Roll20 Contact  :   timmaugh
-Version         :   1.0.1
-Last Update     :   5 DEC 2025
+Version         :   1.0.0
+Last Update     :   12 NOV 2025
 =========================================================
 */
 var API_Meta = API_Meta || {};
@@ -13,10 +13,10 @@ API_Meta.Assemble = { offset: Number.MAX_SAFE_INTEGER, lineCount: -1 };
 
 const Assemble = (() => { // eslint-disable-line no-unused-vars
     const apiproject = 'Assemble';
-    const version = '1.0.1';
+    const version = '1.0.0';
     const schemaVersion = 0.1;
     API_Meta[apiproject].version = version;
-    const vd = new Date(1764944902848);
+    const vd = new Date(1763002255129);
     const apilogo = `https://i.imgur.com/zcfdU8n.png`;
 
     const versionInfo = () => {
@@ -93,25 +93,13 @@ const Assemble = (() => { // eslint-disable-line no-unused-vars
         primaryColor: '#23223F',
         primaryTextColor: '#232323',
         primaryTextBackground: '#ededed',
-        secondaryColor: '#607D8B',
         baseFontFamily: 'Arial',
         baseFontSize: '10pt',
         headerFontFamily: 'Contrail One',
         headerFontSize: '1.3em',
-        headerColor: 'white',
-        dangerColor: `#781718`, // red
-        infoColor: `#1A6675`, // carribean
-        safeColor: `#3E7A46`, // fern
-
+        headerColor: 'white'
     }
     const localCSS = {
-        containerCSS: {
-            'margin-left': '-8px',
-            'width': 'unset',
-            'position': 'relative',
-            'top': '-20px',
-            'display': 'block'
-        },
         msgheader: {
             'background-color': theme.primaryColor,
             'color': theme.headerColor,
@@ -159,11 +147,12 @@ const Assemble = (() => { // eslint-disable-line no-unused-vars
         menubtn: {
             'background-color': theme.primaryColor,
             'border-radius': '6px',
-            'width': '13px',
+            'width': '15px',
             'height': '14px',
             'line-height': '14px',
             'text-decoration': 'none',
             'text-align': 'center',
+            
         },
         buttonPictos: {
             'background-color': theme.primaryColor,
@@ -173,23 +162,9 @@ const Assemble = (() => { // eslint-disable-line no-unused-vars
             'font-family': 'pictos',
             'font-size': '12px',
             'text-align': 'center',
-            'width': '13px',
+            'width': '15px',
             'height': '14px',
             'vertical-align': 'middle',
-            'margin-top': '-2px',
-            'text-decoration': 'none',
-        },
-        secondaryColor: {
-            'background-color': theme.secondaryColor
-        },
-        danger: {
-            'background-color': theme.dangerColor
-        },
-        safe: {
-            'background-color': theme.safeColor
-        },
-        info: {
-            'background-color': theme.infoColor
         },
         tipContainer: {
             'overflow': 'hidden',
@@ -276,7 +251,7 @@ const Assemble = (() => { // eslint-disable-line no-unused-vars
         btn: btn = '',
     } = {}) => {
         if (title) title = html.div(html.div(html.img(apilogo, 'Assemble Logo', localCSS.logoimg), localCSS.msgheaderlogodiv) + html.div(title, localCSS.msgheadercontent), {});
-        Messenger.MsgBox({ msg: msg, title: title, bodycss: bodycss, sendas: sendas, whisperto: whisperto, footer: footer, btn: btn, headercss: headercss, footercss: footercss, boundingcss: localCSS.boundingcss, containercss: localCSS.containerCSS, noarchive: true });
+        Messenger.MsgBox({ msg: msg, title: title, bodycss: bodycss, sendas: sendas, whisperto: whisperto, footer: footer, btn: btn, headercss: headercss, footercss: footercss, boundingcss: localCSS.boundingcss, noarchive: true });
     };
     const getTip = (label, header = 'Info', contents, contentcss = {}) => {
         let contentCSS = { ...localCSS.tipContent, ...contentcss };
@@ -303,29 +278,11 @@ const Assemble = (() => { // eslint-disable-line no-unused-vars
         );
     };
 
-    const getWhisperTo = (who) => who.toLowerCase() === 'api'
-        ? 'gm'
-        : who.toLowerCase() === 'all'
-            ? ''
-            : who.replace(/\s\(gm\)$/i, '');
+    const getWhisperTo = (who) => who.toLowerCase() === 'api' ? 'gm' : who.replace(/\s\(gm\)$/i, '');
     // ==================================================
     //		UTILITIES
     // ==================================================
     const escapeRegExp = (string) => { return string.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&'); };
-    const getAgnostic = query => {
-        let o = findObjs({ type: 'character' }).filter(c => c.get('name') === query || c.id === query)[0] ||
-            findObjs({ type: 'player', displayname: query })[0];
-        if (o) {
-            o.speakerType = o.get('type');
-            o.localName = o.get('name') || o.get('displayname');
-            o.chatSpeaker = o.speakerType + '|' + o.id;
-        } else if (query.toLowerCase() === 'all') {
-            o = { id: undefined, type: 'all', localName: 'all', speakerType: 'all', chatSpeaker: 'all', get: () => { return 'all'; } };
-        } else {
-            o = { id: undefined, type: 'gm', localName: 'gm', speakerType: 'gm', chatSpeaker: 'gm', get: () => { return 'gm'; } };
-        }
-        return o;
-    };
     const getTheSpeaker = msg => {
         let speaking;
         if (['API', ''].includes(msg.who)) {
@@ -395,13 +352,7 @@ const Assemble = (() => { // eslint-disable-line no-unused-vars
         if (!attr && create) attr = createObj('attribute', { characterid: char.id, name: aname });
         return attr;
     };
-
-    // ==================================================
-    //		DEFINITIONS & TABLES
-    // ==================================================
-    const posValues = ['yes', 'y', 'true', 't', 'keith', 'yep'];
     const maxAttrPrefix = `Assemble Attr: `;
-
     // ==================================================
     //		HANDLE INPUT
     // ==================================================
@@ -420,8 +371,6 @@ const Assemble = (() => { // eslint-disable-line no-unused-vars
             .map(a => [a[0], /^(['`"])(.*)\1$/.test(a[1]) ? /^(['`"])(.*)\1$/.exec(a[1])[2] : a[1]])
             .filter(a => a[1] && a[1].length);
 
-        let theSpeaker = getTheSpeaker(msg);
-
         let argObj = {
             delim: ',',
             keep: 'id',
@@ -431,16 +380,13 @@ const Assemble = (() => { // eslint-disable-line no-unused-vars
             count: 0,
             targets: [],
             menu: [],
-            swapdelim: undefined,
-            report: true,
-            unique: true,
-            who: theSpeaker
+            swapdelim: undefined
         };
         let modeArray = ['=', '+', '-'];
         let rxRet;
         let explicitDelim = false;
         let explicitKeep = false;
-        let explicitUnique = false;
+        let origDataCount = 0;
         args.forEach(a => {
             switch (a[0]) {
                 case 'delim':
@@ -484,30 +430,15 @@ const Assemble = (() => { // eslint-disable-line no-unused-vars
                         argObj.swapdelim = a[1];
                     }
                     break;
-                case 'report':
-                    if (!posValues.includes(a[1])) {
-                        argObj.report = false;
-                    }
-                    break;
-                case 'unique':
-                    if (!posValues.includes(a[1])) {
-                        argObj.unique = false;
-                        explicitUnique = true;
-                    }
-                    break;
-                case 'who':
-                    if (a[1].length) {
-                        argObj.who = getAgnostic(a[1]);
-                    }
-                    break;
                 default:
             }
         });
 
+        let theSpeaker = getTheSpeaker(msg);
 
         // CONFIRM CHARACTER AVAILABILITY =======================================
         if ((argObj.count || argObj.targets.length) && (!argObj.char || !argObj.char.id)) {
-            if (argObj.who.speakerType === 'character') {
+            if (theSpeaker.speakerType === 'character') {
                 argObj.char = theSpeaker;
             } else {
                 msgbox({
@@ -528,7 +459,11 @@ const Assemble = (() => { // eslint-disable-line no-unused-vars
         if (argObj.char && argObj.char.id && argObj.attr && argObj.attr.length) {
 
             let attr = obtainAttr(argObj.attr, argObj.char, msg.playerid);
-            let data;
+            let data = [...new Set(argObj.targets)]
+                .map(t => getToken(t))
+                .filter(t => t && t.id)
+                .map(t => retFuncs[argObj.keep](t));
+            origDataCount = data.length;
 
             if (!explicitDelim && attr.get('max').length && /\(delim:([^)]+)\)/.test(attr.get('max'))) {
                 argObj.delim = /\(delim:([^)]+)\)/.exec(attr.get('max'))[1];
@@ -536,52 +471,34 @@ const Assemble = (() => { // eslint-disable-line no-unused-vars
             if (!explicitKeep && attr.get('max').length && /\(keep:(id|name|charid)\)/i.test(attr.get('max'))) {
                 argObj.keep = /\(keep:([^)]+)\)/.exec(attr.get('max'))[1].toLowerCase();
             }
-            if (!explicitUnique && attr.get('max').length && /\(unique:([^)]+)\)/i.test(attr.get('max'))) {
-                argObj.unique = /\(unique:([^)]+)\)/.exec(attr.get('max'))[1].toLowerCase();
-            }
 
-            if (argObj.swapdelim) {
-                data = [...(argObj.unique ? new Set([...attr.get('current').split(argObj.delim)]) : attr.get('current').split(argObj.delim))].filter(d => d && d.length);
-                argObj.delim = argObj.swapdelim;
-                attr.set({ current: data.join(argObj.delim), max: `${maxAttrPrefix}(delim:${argObj.delim})(keep:${argObj.keep})` });
-                msgbox({
-                    title: 'Delimiter Swapped',
-                    msg: `Changed delimiter for ${data.length} entries.`,
-                    btn: Messenger.Button({ type: '!', elem: `!assemble --menu|${argObj.char.get('name')}`, label: 'Menu', css: [localCSS.btncss] }),
-                    whisperto: getWhisperTo(argObj.who.localName)
-                });
-            }
-
-            data = [...(argObj.unique ? new Set(argObj.targets) : argObj.targets)]
-                .map(t => getToken(t))
-                .filter(t => t && t.id)
-                .map(t => retFuncs[argObj.keep](t));
-
-            if (data.length) {
+            if (data.length || argObj.swapdelim) {
                 switch (argObj.mode) {
                     case '+':
-                        data = [...(argObj.unique ? new Set([...attr.get('current').split(argObj.delim), ...data]) : [...attr.get('current').split(argObj.delim), ...data])].filter(d => d && d.length);
+                        data = [...new Set([...attr.get('current').split(argObj.delim), ...data])].filter(d => d && d.length);
                         break;
                     case '-':
-                        data = attr.get('current').split(argObj.delim).filter(d => !data.includes(d));
+                        data = [...new Set([...attr.get('current').split(argObj.delim)])].filter(d => !data.includes(d));
                         break;
                     case '=':
                     default:
                 }
 
-                attr.set({ current: data.join(argObj.delim), max: `${maxAttrPrefix}(delim:${argObj.delim})(keep:${argObj.keep})(unique:${argObj.unique})` });
-
-                if (argObj.report) {
-                    msgbox({
-                        title: 'Process Completed',
-                        msg: `${argObj.mode === '+' ? 'Added' :
-                            argObj.mode === '-' ? 'Removed' : 'Overwrote with'} ${data.length} entries. Current contents are shown below.` +
-                            html.tag('hr', '', { 'border-top-color': theme.primaryColor }) +
-                            HE(attr.get('current')),
-                        btn: Messenger.Button({ type: '!', elem: `!assemble --menu|${argObj.char.get('name')}`, label: 'Menu', css: [localCSS.btncss] }),
-                        whisperto: getWhisperTo(argObj.who.localName)
-                    });
+                if (argObj.swapdelim) {
+                    argObj.delim = argObj.swapdelim;
                 }
+
+                attr.set({ current: data.join(argObj.delim), max: `${maxAttrPrefix}(delim:${argObj.delim})(keep:${argObj.keep})` });
+
+                msgbox({
+                    title: 'Process Completed',
+                    msg: `${argObj.mode === '+' ? 'Removed' :
+                        argObj.mode === '-' ? 'Added' : 'Overwrote with'} ${data.length} entries${argObj.swapdelim ? ' using the new delimiter.' : ''}. Current contents are shown below.` +
+                        html.tag('hr', '', { 'border-top-color': theme.primaryColor }) +
+                        HE(attr.get('current')),
+                    btn: Messenger.Button({ type: '!', elem: `!assemble --menu|${argObj.char.get('name')}`, label: 'Menu', css: [localCSS.btncss] }),
+                    whisperto: getWhisperTo(theSpeaker.localName)
+                });
             }
 
             // SEND NEW TARGETING MESSAGE =========================================
@@ -599,14 +516,12 @@ const Assemble = (() => { // eslint-disable-line no-unused-vars
                 if (argObj.delim !== ',') { reconArgs.delim = `--delim|'${argObj.delim}'`; }
                 if (argObj.keep !== 'id') { reconArgs.keep = `--keep|${argObj.keep}`; }
                 if (argObj.mode !== '=') { reconArgs.mode = `--mode|${argObj.mode}`; }
-                if (!argObj.report) { reconArgs.report = `--report|no`; }
-                if (!argObj.unique) { reconArgs.unique = `--unique|no`; }
                 reconArgs.targets = Array(argObj.count).fill().map((a, i) => `--target|@{target|Target ${i + 1}|token_id}`).join(' ');
 
                 let outbound = `Click the button to choose the ${argObj.count} target${argObj.count > 1 ? 's' : ''} that will ` +
                     `${actionText[argObj.mode]} the ${argObj.attr} attribute on ${argObj.char.get('name')}.`;
                 let btn = Messenger.Button({ label: '&nbsp;Assemble!&nbsp;', elem: `!assemble ${reconArgs.toArgs()}`, type: 'api', css: localCSS.btncss });
-                msgbox({ title: 'Targets Assemble!', msg: outbound, whisperto: getWhisperTo(argObj.who.localName), btn: btn });
+                msgbox({ title: 'Targets Assemble!', msg: outbound, whisperto: getWhisperTo(theSpeaker.localName), btn: btn });
 
             }
         }
@@ -617,35 +532,30 @@ const Assemble = (() => { // eslint-disable-line no-unused-vars
                 .filter(a => (new RegExp(`^${escapeRegExp(maxAttrPrefix)}`)).test(a.get('max')));
 
             let hdr = html.tr(
-                html.th('ATTR', localCSS.textleft) + html.th('ACTIONS') // + html.th('REM') + html.th('OVER') + html.th('DELIM')
+                html.th('ATTR', localCSS.textleft) + html.th('ADD') + html.th('REM') + html.th('OVER') + html.th('DELIM')
             );
             let rows = menuAttrs.map(a => {
-                let locDelim = a.get('max').length && /\(delim:([^)]+)\)/.test(a.get('max'))
-                    ? argObj.delim = /\(delim:([^)]+)\)/.exec(a.get('max'))[1]
-                    : argObj.delim;
-                let cmdStart = `!assemble --group|${argObj.menu.get('name')}|${a.get('name')} --delim|'${argObj.delim}' --keep|${argObj.keep} --unique|${argObj.unique}`;
+                let cmdStart = `!assemble --group|${argObj.menu.get('name')}|${a.get('name')} --delim|'${argObj.delim}' --keep|${argObj.keep}`;
                 return html.tr(
-                    html.td(getTip(a.get('name'), `${a.get('current').split(argObj.delim).length} Entries`, HE(a.get('current'))), localCSS.textleft) +
-                    html.td(
-                        Messenger.Button({ type: '!', elem: `${cmdStart} --mode|+ --target|@{target|Target 1|token_id}`, label: html.tip('+1', 'Add one'), css: [localCSS.menubtn, localCSS.safe] }) +
-                        Messenger.Button({ type: '!', elem: `${cmdStart} --count|+?{How many targets|1|2|3|4|5|6|7|8|9|10}`, label: html.tip('+?', 'Add by query'), css: [localCSS.menubtn, localCSS.safe] }) +
-                        Messenger.Button({ type: '!', elem: `${cmdStart} --mode|- --target|@{target|Target 1|token_id}`, label: html.tip('-1', 'Remove one'), css: [localCSS.menubtn, localCSS.danger] }) +
-                        Messenger.Button({ type: '!', elem: `${cmdStart} --count|-?{How many targets|1|2|3|4|5|6|7|8|9|10}`, label: html.tip('-?', 'Remove by query'), css: [localCSS.menubtn, localCSS.danger] }) +
-                        Messenger.Button({ type: '!', elem: `${cmdStart} --count|=?{How many targets|1|2|3|4|5|6|7|8|9|10}`, label: html.tip('=?', 'Overwrite by query'), css: [localCSS.menubtn, localCSS.secondaryColor] }) +
-                        Messenger.Button({ type: '!', elem: `!assemble --group|${argObj.menu.get('name')}|${a.get('name')} --swapdelim|'?{Enter new delimiter|${locDelim}}'`, label: html.tip('y', 'Swap delimiter'), css: [localCSS.buttonPictos] })
-                    )
+                    html.td(getTip(a.get('name'), `${a.get('current').split(argObj.delim).length} Entries`, HE(a.get('current'))),localCSS.textleft) +
+                    html.td(Messenger.Button({ type: '!', elem: `${cmdStart} --mode|+ --target|@{target|Target 1|token_id}`, label: html.tip('+1', 'Add one'), css: [localCSS.menubtn] }) +
+                        Messenger.Button({ type: '!', elem: `${cmdStart} --count|+?{How many targets|1|2|3|4|5|6|7|8|9|10}`, label: html.tip('+?', 'Add by query'), css: [localCSS.menubtn] })) +
+                    html.td(Messenger.Button({ type: '!', elem: `${cmdStart} --mode|- --target|@{target|Target 1|token_id}`, label: html.tip('-1', 'Remove one'), css: [localCSS.menubtn] }) +
+                        Messenger.Button({ type: '!', elem: `${cmdStart} --count|-?{How many targets|1|2|3|4|5|6|7|8|9|10}`, label: html.tip('-?', 'Remove by query'), css: [localCSS.menubtn] })) +
+                    html.td(Messenger.Button({ type: '!', elem: `${cmdStart} --count|=?{How many targets|1|2|3|4|5|6|7|8|9|10}`, label: html.tip('=?', 'Overwrite by query'), css: [localCSS.menubtn] })) +
+                    html.td(Messenger.Button({ type: '!', elem: `!assemble --swapdelim|'?{Enter new delimiter|${argObj.delim}'`, label: html.tip('y', 'Swap delimiter'), css: [localCSS.buttonPictos] }))
                 );
             });
             let ftr = html.tr(
                 html.tdcs(Messenger.Button({
                     type: '!',
-                    elem: `!assemble --group|${argObj.menu.get('name')}|?{Name for attribute} --delim|'?{List delimiter|,}' --keep|?{Save info|Token ID,id|Name,name|Character ID,charid} --unique|?{Allow Duplicates?|No,yes|Yes,no} --count|=?{How many targets?|1|2|3|4|5|6|7|8|9|10}`,
+                    elem: `!assemble --group|${argObj.menu.get('name')}|?{Name for attribute} --delim|'?{List delimiter|,}' --keep|?{Save info|Token ID,id|Name,name|Character ID,charid} --count|=?{How many targets?|1|2|3|4|5|6|7|8|9|10}`,
                     label: 'Create New',
                     css: [localCSS.btncss]
                 }), 5, {'text-align': 'right'})
             )
             let tbl = html.table(hdr+rows.join('')+ftr,localCSS.textcenter);
-            msgbox({ title: argObj.menu.get('name'), msg: tbl, whisperto: getWhisperTo(argObj.who.localName) });
+            msgbox({ title: argObj.menu.get('name'), msg: tbl, whisperto: getWhisperTo(theSpeaker.localName) });
         }
     };
 
